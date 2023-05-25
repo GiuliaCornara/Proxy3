dfa9d2be183a0e2fa724968c0a4e9a49250d95aa
GiuliaCornara <132066433+GiuliaCornara@users.noreply.github.com>
2023-05-25 15:09:27 +0200

    Update main.py

--- project_codebase-master/main.py
+++ project_codebase-master/main.py
@@ -121,7 +121,14 @@ class LightningModel(pl.LightningModule):
         
         
         self.loss_unsupervised=losses.VICRegLoss()
-        self.augmentation = TrainTransform()
+        self.augmentation = self.transform = tfm.Compose( [tfm.RandomResizedCrop(224, interpolation=InterpolationMode.BICUBIC),
+                tfm.RandomHorizontalFlip(p=0.5),
+                tfm.RandomApply([tfm.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1 ) ], p=0.8 ),
+                tfm.RandomGrayscale(p=0.2),
+                GaussianBlur(p=1.0),
+                Solarization(p=0.0),
+                tfm.ToTensor(),
+                tfm.Normalize( mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225] ) ] )
         
         
         # Set the miner
